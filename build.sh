11058a46602463167272ca13f1492b0a8fb11ff3
Adrian Winterstein <adrian@winterstein.biz>
2025-01-06 12:52:34 +0100

    Allow different CSS file for the website (updated from theme).

--- build.sh
+++ build.sh
@@ -3,26 +3,13 @@
 # get the directory in which this script is located (the theme source dir)
 SOURCE_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
-ZOLA_COMMAND="zola build"
+# create the container for zola builds and tailwindcss updates
+docker build -t develop-zola-tailwindcss "${SOURCE_DIR}/themes/project-portfolio" || exit 2
 
-# call the zola executable, if it can be found in the path
-if command -v zola >/dev/null; then
-    pushd "${SOURCE_DIR}" >/dev/null
-    ${ZOLA_COMMAND}
-    popd >/dev/null
+# update the CSS file using tailwindcss
+docker run -v ${SOURCE_DIR}:/source:Z -w /source develop-zola-tailwindcss \
+    tailwindcss -i themes/project-portfolio/src/css/main.css -o static/css/main.css --minify || exit 3
 
-# alternatively use docker to create and run a container and execute zola from there
-elif command -v docker >/dev/null; then
-
-    # create the container for zola builds and tailwindcss updates
-    docker build -t develop-zola-tailwindcss "${SOURCE_DIR}/themes/project-portfolio" || exit 2
-
-    # build the website via zola
-    docker run -v ${SOURCE_DIR}:/source:Z -w /source develop-zola-tailwindcss \
-        ${ZOLA_COMMAND} || exit 3
-
-# print an error message if neither tailwindcss nor the docker executable could be found
-else
-    echo "Need either 'zola' or 'docker' executable in the path." >&2
-    exit 1
-fi
+# build the website via zola
+docker run -v ${SOURCE_DIR}:/source:Z -w /source develop-zola-tailwindcss \
+    zola build || exit 3
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    'themes/project-portfolio/templates/**/*.html',
+    'themes/project-portfolio/static/js/**/*.js',
+    'content/**/*.md',
+    'templates/**/*.html',
+    'static/js/**/*.js',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
