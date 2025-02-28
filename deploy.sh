f438578b83a245adc5b2e0333269b174b43beb36
Adrian Winterstein <adrian@winterstein.biz>
2025-02-28 14:30:45 +0100

    Updated deploy script for new hosting provider.

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,5 @@ SOURCE_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 # build the website first
 "$SOURCE_DIR/build.sh"
 
-# import the FTP configuration (the file must include HOST, USER and PASS variables)
-# shellcheck source=/dev/null
-source "$SOURCE_DIR/.ftp-config"
-
-# define local and remote directories
-LCD="$SOURCE_DIR/public"
-RCD="/winterstein.biz"
-
-lftp -f "
-open $HOST
-user $USER $PASS
-lcd $LCD
-mirror --continue --reverse --delete --verbose $LCD $RCD
-bye
-"
+# sync the files to the web hosting
+rsync -r --delete public/ ngcobalt447.manitu.net:/home/sites/site100040730/web/winterstein.biz/
